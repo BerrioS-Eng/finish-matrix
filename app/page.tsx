be892cd110292f5b3106d5850ac52efabd8c056d
Sebastian Berrio <sberrio09@gmail.com>
2023-06-25 23:37:03 -0500

    W03 page home section3-links

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import { Slider } from "@/components/slider/Slider"
 import styles from "./Page.module.css"
 import Link from "next/link"
 import Image from "next/image"
@@ -6,16 +5,15 @@ import Image from "next/image"
 export default function Home() {
   return (
     <>
-
       <div className="">
         <div className={styles.section1}>
           <section className={styles.home}>
-            <h3 className="">Matriz<br />Genérica de<br />Acabados</h3>
-            <p className="">
+            <h3 className={styles.p1}>Matriz<br />Genérica de<br />Acabados</h3>
+            <p className={styles.paragraph}>
               Los materiales de construcción provienen de diversos orígenes,
               vistos desde la naturaleza o esencia misma de su estructura.
             </p>
-            <p>
+            <p className={styles.paragraph}>
               Comprender que la manera de abordarlos resulta muy amplia es conveniente para el entendimiento
               proyectual, debido a esto se construye un sistema de clasificación, que permite analizar el
               material en cuestión desde la naturaleza física de su composición hasta sus índices de
@@ -26,53 +24,40 @@ export default function Home() {
             <Image className={styles.imageHome} src="/matrixdeg.png" alt="matrixHome" width={848} height={548}></Image>
           </div>
         </div>
-
-        <section>
-          <Slider />
+        <section className={styles.section2}>
+          <div>
+            <h3>Sistema de clasificación y categorización de acabados arquitectonicos</h3>
+            <p className={styles.paragraph}>
+              Se muestra a partir de fichas de identidad, desglosando información de los materiales como el aspecto
+              físico y compositivo, también, el comportamiento frente a diferentes criterios de sostenibilidad
+              bioclimática.
+              <br />
+              Se establece en primera instancia dos grupos: sólidos y fluidos; luego se subdividen por
+              su naturaleza, de esta manera se obtiene un estudio amplio y adecuado en los aspectos claves
+              para la escogencia de los materiales, consiguiendo un sistema aplicable a lo largo del tiempo.
+            </p>
+          </div>
         </section>
-        
-        <div className={styles.section2}>
-          <section>
-            <div className="">
-              <h2 className="">Herramienta para determinar y seleccionar acabados en fachadas arquitectónicas</h2>
-              <p className="">Se muestra a partir de fichas de identidad, desglosando información de los materiales como el aspecto físico y compositivo, también, el comportamiento frente a diferentes criterios de sostenibilidad bioclimática.<br />Estableciendo en primera instancia dos grandes grupos: sólidos y fluidos; luego subdividirlos por su naturaleza misma. </p>
-            </div>
-            <div className="">
-              <div className="">
-                <div className="">
-                  <Link href="/solidos" className="">
-                    <div className="">
-                      <div className="icon">
-                        <div className="">
-                          <i className="fa fa-duotone fa-shop fa-4x"></i>
-                        </div>
-                        Sólidos
-                      </div>
-                    </div>
-                  </Link>
-
-                </div>
-                <div className="">
-
-                  <Link href="/fluidos" className="w-50 pl-3 text-decoration-none">
-                    <div className="">
-                      <div className="icon">
-                        <div className="">
-                          <i className="fa fa-duotone fa-water fa-4x"></i>
-                        </div>
-                        Fluidos
-                      </div>
-                    </div>
-                  </Link>
-                </div>
+        <section className={styles.section3}>
+          <Link href="">
+            <div className={styles.iconSolid}>
+              <div>
+                <i className="fa fa-duotone fa-shop fa-4x"></i>
               </div>
+              Sólidos
             </div>
-          </section>
-        </div>
-
+          </Link>
+          <Link href="">
+            <div className={styles.iconFluid}>
+              <div>
+                <i className="fa fa-duotone fa-water fa-4x"></i>
+              </div>
+              Fluidos
+            </div>
+          </Link>
+        </section>
 
       </div>
-
     </>
   )
 }
--- components/slider/Slider.tsx
+++ components/slider/Slider.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import styles from "./Slider.module.css"
 
 export const Slider = () => {
@@ -5,32 +6,32 @@ export const Slider = () => {
     <div className={styles.slider}>
       <div className={styles.container}>
         <div className={styles.carousel}>
-          <div className={styles.carousel__face}>
-            
+          <div className={styles.carousel__item}>
+            <Image src={"/acabado1.jpg"} alt="" width={0} height={0} />
           </div>
-          <div className={styles.carousel__face}>
+          <div className={styles.carousel__item}>
             <span className={styles.span}>Revestimientos</span>
           </div>
-          <div className={styles.carousel__face}>
-
+          <div className={styles.carousel__item}>
+            <Image src={"/acabado2.jpg"} alt="" width={0} height={0} />
           </div>
-          <div className={styles.carousel__face}>
-
+          <div className={styles.carousel__item}>
+            <Image src={"/acabado4.jpg"} alt="" width={0} height={0} />
           </div>
-          <div className={styles.carousel__face}>
+          <div className={styles.carousel__item}>
             <span className={styles.span}>Recubrimientos</span>
           </div>
-          <div className={styles.carousel__face}>
-
+          <div className={styles.carousel__item}>
+            <Image src={"/acabado6.jpg"} alt="" width={0} height={0} />
           </div>
-          <div className={styles.carousel__face}>
-
+          <div className={styles.carousel__item}>
+            <Image src={"/acabado7.jpg"} alt="" width={0} height={0} />
           </div>
-          <div className={styles.carousel__face}>
+          <div className={styles.carousel__item}>
             <span className={styles.span}>Cerramientos</span>
           </div>
-          <div className={styles.carousel__face}>
-
+          <div className={styles.carousel__item}>
+            <Image src={"/acabado19.jpg"} alt="" width={0} height={0} />
           </div>
         </div>
       </div>
